0a9757faecc7d4b63be3ccbb478640320ac7af9d
fjodoin <143029509+fjodoin@users.noreply.github.com>
2023-09-30 15:56:08 -0400

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys
+import json
+import requests
+import argparse
+from bs4 import BeautifulSoup
+# Code built from HTB modules
+
+def results(file):
+        content=open(file,'r').readlines()
+        for line in content:
+                data=json.loads(line.strip())
+        urls=[]
+        for url in data['results']:
+                urls.append(url['url'])
+        return urls
+def crawl(url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,'html.parser')
+        links = soup.findAll('a',href=True)
+        for link in links:
+                link=link['href']
+                if link and link!='#':
+                        print '[+] {} : {}'.format(url,link)
+if __name__=="__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument("file",help="ffuf results")
+        args = parser.parse_args()
+        urls=results(args.file)
+        for url in urls:
+                crawl(url)
